e3b070871f7bf79ff457f83a4a473d9a197944cf
Joe Moore <joe.moore@siemens.com>
2018-05-03 15:48:22 -0400

    Project created with https://github.com/jpvlsmv/cookiecutter-python-cli

---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+Remove files from target_dir if reference_dir has a copy
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='rmdups',
+    version='0.1.0',
+    url='https://github.com/jpvlsmv/python-rmdups',
+    license='BSD',
+    author='Joe Moore',
+    author_email='jpvlsmv@gmail.com',
+    description='Remove files from target_dir if reference_dir has a copy',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'rmdups = rmdups.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
