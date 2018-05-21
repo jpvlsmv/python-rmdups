72f3a672ff19c2c11c481e298cbc84efc51c1366
Joe Moore <jpvlsmv@gmail.com>
2018-05-20 22:03:18 -0400

    write byte strings into the files

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -46,22 +46,22 @@ def mk_files():
     r = Path('reference')
     r.mkdir()
     with open(r / "file2.txt", 'wb') as f:
-        f.write('Duplicate detected')
+        f.write(b'Duplicate detected')
     with open(r / "files.md5sum", 'wb') as f:
-        f.write('4d034101596def954ebccf4e7275cb43  file2.txt')
+        f.write(b'4d034101596def954ebccf4e7275cb43  file2.txt')
     with open(r / "files.sha256sum", 'wb') as f:
         f.write(
-                '5207f1a1f5390a25358aa34969e2aeb275180bce7c62db8c4c64237dee722f52  file2.txt\n'
-                'df24ef8c58e9754070346a01d6aefe6792f1e800d2dbc74dface6ecb93f537f9  files.md5sum'
+                b'5207f1a1f5390a25358aa34969e2aeb275180bce7c62db8c4c64237dee722f52  file2.txt\n'
+                b'df24ef8c58e9754070346a01d6aefe6792f1e800d2dbc74dface6ecb93f537f9  files.md5sum\n'
                 )
     t = Path('target')
     t.mkdir()
     with open(t / "file1.txt", 'wb') as f:
-        f.write('Hello World')
+        f.write(b'Hello World')
     with open(t / "file2.txt", 'wb') as f:
-        f.write('Duplicate detected')
+        f.write(b'Duplicate detected')
     with open(t / "file3.txt", 'wb') as f:
-        f.write('Duplicate detected')
+        f.write(b'Duplicate detected')
 
 
 def test_reference(runner):
