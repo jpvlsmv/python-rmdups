b5a02dae72e36a911afdd40ce3513f72446135b2
Joe Moore <jpvlsmv@gmail.com>
2018-05-20 22:08:12 -0400

    pathsep problems

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -31,13 +31,13 @@ def test_reference_detection_byhash(runner):
                                 "-r", "reference"])
         assert not result.exception
         assert ''.join(result.output.split()) == ''.join(r'''
-Operating on reference file reference\files.md5sum
+Operating on reference file reference/files.md5sum
 Read 1 hashes
 Working with target directory target
-target\file1.txt
-target\file2.txt
+target/file1.txt
+target/file2.txt
 4d034101596def954ebccf4e7275cb43 seen before
-target\file3.txt
+target/file3.txt
 4d034101596def954ebccf4e7275cb43 seen before
             '''.split())
 
@@ -46,9 +46,9 @@ def mk_files():
     r = Path('reference')
     r.mkdir()
     with open(r / "file2.txt", 'wb') as f:
-        f.write(b'Duplicate detected')
+        f.write(b'Duplicate detected\n')
     with open(r / "files.md5sum", 'wb') as f:
-        f.write(b'4d034101596def954ebccf4e7275cb43  file2.txt')
+        f.write(b'4d034101596def954ebccf4e7275cb43  file2.txt\n')
     with open(r / "files.sha256sum", 'wb') as f:
         f.write(
                 b'5207f1a1f5390a25358aa34969e2aeb275180bce7c62db8c4c64237dee722f52  file2.txt\n'
@@ -57,11 +57,11 @@ def mk_files():
     t = Path('target')
     t.mkdir()
     with open(t / "file1.txt", 'wb') as f:
-        f.write(b'Hello World')
+        f.write(b'Hello World\n')
     with open(t / "file2.txt", 'wb') as f:
-        f.write(b'Duplicate detected')
+        f.write(b'Duplicate detected\n')
     with open(t / "file3.txt", 'wb') as f:
-        f.write(b'Duplicate detected')
+        f.write(b'Duplicate detected\n')
 
 
 def test_reference(runner):
@@ -72,12 +72,12 @@ def test_reference(runner):
                                ["-r", "reference", "target"])
         assert not result.exception
         assert ''.join(result.output.split()) == ''.join(r'''
-Operating on reference file reference\files.sha256sum
+Operating on reference file reference/files.sha256sum
 Read 2 hashes
 Working with target directory target
-target\file1.txt
-target\file2.txt
-target\file3.txt
+target/file1.txt
+target/file2.txt
+target/file3.txt
             '''.split())
 
 
@@ -88,10 +88,10 @@ def test_hash_mismatch(runner):
                                ["-R", "reference/files.md5sum", "target"])
         assert not result.exception
         assert ''.join(result.output.split()) == ''.join(r'''
-Operating on reference file reference\files.md5sum
+Operating on reference file reference/files.md5sum
 Read 1 hashes
 Working with target directory target
-target\file1.txt
-target\file2.txt
-target\file3.txt
+target/file1.txt
+target/file2.txt
+target/file3.txt
             '''.split())
